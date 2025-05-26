934e48b1d14f8788140979b924a0b0eb6e7aa0f5
Neil <neilmathewsuby@gmail.com>
2025-05-27 03:46:01 +0530

    sybau

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import styles from "./style";
 
 
-function App() {
+const App = () => {
   return (
-    <div className="bg-primary w-full overflow-hidden">
+    <div className="bg-primary w-full overflow-hidden font-bold underline">
       <h1>Hello,World</h1>
     </div>
-  )
+  );
 }
 
 export default App
