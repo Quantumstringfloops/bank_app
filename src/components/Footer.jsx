8220e3ab69822a66f5785db4d157affe88084ac1
Neil <neilmathewsuby@gmail.com>
2025-05-31 19:43:55 +0530

    Footer img

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { layout } from '../style'
+import { logo } from '../assets'
 
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+const Footer = () => (
+    <section className={layout.section}>
+      <div className={``}>
+        <img src={logo} alt="logo" className=' w-[100%] h-[100%]' />
+      </div>
+    </section>
+)
 
 export default Footer
